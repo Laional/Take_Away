e6e4e8b4e8b76470c09f48feb35174c53a4272e5
DESKTOP-H9ASNR5\Konatan <cdokonstan@gmail.com>
2017-04-29 02:23:56 +0200

    - Inicializamos la función MOSTRARCATEGORIAS(); que se usara para visualizar las categorias dentro del MODAL modPlato(); - Inicializamos la función que nos permite hacer funcionar las ventanas MODAL. - Borramos el codigo antiguo y lo sustituimos por uno nuevo. - Creamos un evento para capturar el archivo que seleccionamos en el FILE-INPUT. - Creamos una función para visualizar la imagen dentro de la ventana MODAL de modPlato(); function verImg(evt){...} - Creamos un AJAX para recibir todos los platos desde la base de datos.    - Creamos y asignamos cada campo de nuestra base de datos a una variable.    - Creamos un IF/ELSE para sustituir el texto que aparece en la casilla DISPONIBLE por un ICONO.    - Montamos nuestra tabla para que nos la pinte automaticamente cada vez que entramos en la pagina.    - Finalmente le decimos que nos pinte la tabla dentro de la tabla con el ID #listaPlatos

--- js/listaPlato.js
+++ js/listaPlato.js
@@ -1,65 +1,93 @@
 $(document).ready(function () {
+	// Inicializamos la funcion para conseguir categorias de la base de datos.
+	mostrarCategorias();
+	// Inicializamos la funcion MODAL en para documento correspondiente.
+	$('.modal').modal();
 	//declarar la variable para depurar y no visualizar console.log
 	var debug = false;
-	$.ajax({
-		// Archivo que te trata los datos
-		url: '../php/getListaPlato.php',
-		// Forma de enviar los datos
-		type: 'GET',
-		// Tipo de datos que se envian
-		dataType: 'json',
-		// La variable donde estan los datos a tratar. (datos enviados)
-		//data: jsonData,
-		// Imprecindible para saber si hay errores i/o buen funcionamiento
-		// Funcion que se ejecuta cuando ha funcionado la llamada ajax correctamente
-		success: function (result) {
-			if (debug) {
-				console.log(result.error);
-				console.log(result.query);
-				console.log(result.resultado);
-			}
-				var tbl_body = "";
-				var estado = "Disponible";
-				var estado2 = "No disponible";
-				if (debug) console.log("Pintando");
-				$.each(result.query, function () {
-					var tbl_row = "";
-					$.each(this, function (campo, valor) {
-						switch (campo) {
-							case "Precio_plato":
-									tbl_row += "<td>" +
-										valor +
-										"€" +
-										"</td>";
-								break;
-							case "Foto_plato":
-								tbl_row += "<td>" +
-									"<img class='z-depth-4' src='../img/platos/" +
-									valor +
-									"'width='90px'" +
-									"</td>";
-								break;
-							case "Disponible_plato":
-								if (valor == "on") {
-									tbl_row += "<td>" + estado + "</td>";
-								} else if (valor == "") {
-									tbl_row += "<td>" + estado2 + "</td>";
-								}
-								break;
-							default:
-								tbl_row += "<td>" + valor + "</td>";
-								if (debug) console.log(tbl_row);
-						}
-					})
-					tbl_body += "<tr>" + tbl_row + "</tr>";
+	// Se prepara evento para capturar el archivo
+    $('input[type=file]').on('change', function(event) { // al cambiar el nombre del archivo en el campo
+        files = event.target.files; // Recoge el objeto archivo en variable files
+        console.log(files);
+    });
 
-					if (debug) console.log(tbl_body);
-				})
-				$("#listaPlatos tbody").html(tbl_body);
-		},
-		error: function (result) {
-			alert("ERROR!!!!!!!");
-			if (debug) console.error(result);
-		}
-	});
+    /*-----------------------------------------------------------------------------------*/
+    function verImg(evt) {
+        files = evt.target.files; // FileList object
+
+        //Obtenemos la imagen del campo "file".
+        for (var i = 0, f; f = files[i]; i++) {
+            //Solo admitimos imágenes.
+            if (!f.type.match('image.*')) {
+                continue;
+            }
+
+            var reader = new FileReader();
+
+            reader.onload = (function(theFile) {
+                return function(e) {
+                    // Creamos la imagen.
+                    document.getElementById("list").innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+                };
+            })(f);
+
+            reader.readAsDataURL(f);
+        }
+    }
+
+    document.getElementById('fotoPlat').addEventListener('change', verImg, false);
+    /*-----------------------------------------------------------------------------------*/
+
+    $.ajax ({
+        // Archivo que te trata los datos
+        url: '../php/getListaPlato.php',
+        // Forma de enviar los datos
+        type: 'GET',
+        // Tipo de datos que se envian
+        dataType: 'json',
+        // La variable donde estan los datos a tratar. (datos enviados)
+        //data: jsonData,
+        // Imprecindible para saber si hay errores i/o buen funcionamiento
+        // Funcion que se ejecuta cuando ha funcionado la llamada ajax correctamente
+        success: function(result){
+            //console.log(result.error);
+            // console.log(result.query);
+            //console.log(result.resultado);
+            var tbl_body = '';
+            var icono_on_off = '';
+
+
+            $.each(result.query, function(CampoNombre, CampoValor){
+
+                var id_plato = CampoValor.ID_plato;
+                var nombrePlato = CampoValor.Nombre_plato;
+                var precioPlato = CampoValor.Precio_plato;
+                var descripPlato = CampoValor.Descripcion_plato;
+                var fotoPlato = CampoValor.Foto_plato;
+                var categoriaPlato = CampoValor.Nombre_categoria;
+                var disponiblePlato = CampoValor.Disponible_plato;
+                if (disponiblePlato == 'on') {
+                    icono_on_off = '<i class="medium center material-icons green-text">visibility</i>';
+                } else {
+                    icono_on_off = '<i class="medium center material-icons red-text">visibility_off</i>';
+                }
+
+                tbl_body += "<tr>";
+                tbl_body += "<td class='center'>"+nombrePlato+"</td>";
+                tbl_body += "<td class='center'>"+precioPlato+"</td>";
+                tbl_body += "<td class='center'> <img class='z-depth-4' src='../img/platos/"+fotoPlato+"' width='90px'> </td>";
+                tbl_body += "<td class='center'>"+icono_on_off+"</td>";
+                tbl_body += "<td class='center'>"+categoriaPlato+"</td>";
+                tbl_body += "<td class='center'><a href='' onClick='verPlato("+JSON.stringify(CampoValor)+");'><i class='small material-icons blue-text'>zoom_in</i></a>";
+                tbl_body += "<td class='center'><a href='' onClick='modificarPlato("+JSON.stringify(CampoValor)+");'><i class='small material-icons blue-text'>tunes</i></a></td>";
+                tbl_body += "<td class='center'><a href='' onClick='borrarPlato("+id_plato+",\""+nombrePlato+"\");'><i class='small material-icons red-text'>delete_forever</i></a></td>";
+                tbl_body += "</tr>";
+            })
+            $("#listaPlatos tbody").html(tbl_body);
+        },
+        error: function(result){
+            alert("ERROR!!!!!!!");
+            //console.error(result);
+        }
+    });
 });
